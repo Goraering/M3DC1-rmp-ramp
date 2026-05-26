89bddcd50a198ead76fbdc9c951c9430c8ccc70d
Seahyun Jo <50688627+Goraering@users.noreply.github.com>
2026-05-26 21:04:20 +0900

    Update m3dc1_matrix.cc

--- m3dc1_scorec/src/m3dc1_matrix.cc
+++ m3dc1_scorec/src/m3dc1_matrix.cc
@@ -1455,6 +1455,9 @@ int matrix_solve::setKspType() {
       ierr = KSPSetOptionsPrefix(_ksp, "hard_");
       ierr = MatViewFromOptions(_A, NULL, "-A_view");
     }
+	if (mymatrix_id == 6) {
+		ierr = KSPSetOptionsPrefix(_ksp, "hardfield_"); 
+	}
   }
 
   ierr = KSPSetFromOptions(_ksp);
